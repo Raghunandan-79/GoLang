78afa6c436003c2b956a4c40c09f8b5cb5c1f171
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-11 09:20:50 +0530

    Variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Golang"
+	fmt.Println(name)
+
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	var age int
+	age = 18
+	fmt.Println(age)
+
+	price := 10.5
+	fmt.Println(price)
+}
