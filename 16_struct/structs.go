a178ac28ff69b7722b8a1507e20a519a1af692fc
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-11 21:59:22 +0530

    Structs

---
+++ 16_struct/structs.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// order struct
+type order struct {
+	id        string
+	amount    float32
+	status    string
+	createdAt time.Time
+}
+
+func (o *order) changeStatus(status string) {
+	o.status = status
+}
+
+func (o order) getStatus() string {
+	return o.status
+} 
+
+func main() {
+	var myOrder order = order{
+		id:     "1",
+		amount: 50.00,
+		status: "received",
+	}
+
+	fmt.Println("Order struct", myOrder)
+
+	myOrder.createdAt = time.Now()
+
+	fmt.Println(myOrder)
+
+	fmt.Println(myOrder.status)
+
+	myOrder2 := order {
+		id: "2",
+		amount: 100,
+		status: "deliverd",
+		createdAt: time.Now(),
+	}
+
+	fmt.Println(myOrder2)
+
+	myOrder2.changeStatus("return")
+
+	fmt.Println(myOrder2.getStatus())
+
+	fmt.Println(myOrder2)
+}
