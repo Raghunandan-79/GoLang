c69f954decbe41a25d3ba33afa63c62cc22b2139
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-12 22:27:54 +0530

    Generics for struct

--- 19_generics/generics.go
+++ 19_generics/generics.go
@@ -8,7 +8,17 @@ func printSlice[T comparable, V string] (items []T, name V) {
 	}
 }
 
+type stack[T any] struct {
+	elements []T
+}
+
 func main() {
 	languages := []string{"golang", "java", "c++"}
 	printSlice(languages, "Raghu")
+
+	stackElements := []int{1, 2, 3, 4, 5}
+	myStack := stack[int]{
+		elements: stackElements,
+	}
+	fmt.Println(myStack)
 }
