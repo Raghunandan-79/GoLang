8b3e29cdd7b68dc30a6d160d19e058338eca4d71
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-13 12:50:36 +0530

    Go routines

---
+++ 20_goroutines/goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// func task(id int) {
+// 	fmt.Println("Executing task", id)
+// }
+
+func main() {
+	// usage of function made outside main
+	// for i := 0; i <= 10; i++ {
+	// 	go task(i)
+	// }
+
+	// using inline function
+	for i := 0; i <= 10; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		} (i)
+	}
+
+	time.Sleep(time.Second * 2)
+}
