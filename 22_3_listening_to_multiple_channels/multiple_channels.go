9a98ff8e40a272a7005913664dcf542932f59914
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-13 14:31:26 +0530

    Listening multiple channels

---
+++ 22_3_listening_to_multiple_channels/multiple_channels.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	chan1 := make(chan int)
+	chan2 := make(chan string)
+
+	go func() {
+		chan1 <- 5
+	}()
+
+	go func() {
+		chan2 <- "pong"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case chan1Val := <- chan1:
+			fmt.Println("Received data from chan1", chan1Val)
+		case chan2Val := <- chan2:
+			fmt.Println("Received data from chan2", chan2Val)
+		}
+	}
+}
