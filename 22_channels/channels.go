60f77e79d2d874b768d2061cfcdff57f597ce071
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-13 13:41:49 +0530

    Receiving data from channel

--- 22_channels/channels.go
+++ 22_channels/channels.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	// import below two when sending data
+	// "math/rand"
+	// "time"
 )
 
 // below two are for sending
@@ -20,17 +21,30 @@ import (
 // }
 
 // with loop
-func processNum(numChan chan int) {
-	for num := range numChan {
-		fmt.Println("Processing number", num)
-		time.Sleep(time.Second * 1)
-	}
+// func processNum(numChan chan int) {
+// 	for num := range numChan {
+// 		fmt.Println("Processing number", num)
+// 		time.Sleep(time.Second * 1)
+// 	}
+// }
+
+// func main() {
+// 	numChan := make(chan int)
+// 	go processNum(numChan)
+// 	for {
+// 		numChan <- rand.Intn(100)
+// 	}
+// }
+
+// receiving data
+func sum(result chan int, num1 int, num2 int) {
+	sumResult := num1 + num2
+	result <- sumResult
 }
 
 func main() {
-	numChan := make(chan int)
-	go processNum(numChan)
-	for {
-		numChan <- rand.Intn(100)
-	}
+	result := make(chan int)
+	go sum(result, 4, 5)
+	res := <- result
+	fmt.Println(res)
 }
