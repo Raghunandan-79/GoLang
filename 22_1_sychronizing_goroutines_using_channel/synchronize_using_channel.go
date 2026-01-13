314e4449a96a0c92e9d224cdf54b3f93812c60b7
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-01-13 13:55:28 +0530

    Synchronizing goroutines using channels

---
+++ 22_1_sychronizing_goroutines_using_channel/synchronize_using_channel.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func task(done chan bool) {
+	defer func() { done <- true }()
+	fmt.Println("Processing...")
+}
+
+func main() {
+	done := make(chan bool)
+	go task(done)
+	<- done
+}
